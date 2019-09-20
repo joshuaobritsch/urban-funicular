c249f5d731d70ac698e286030ed64dd5ae921821
Joshua Obritsch <j.obritsch@gmail.com>
2019-09-20 15:16:47 +0200

    Replace global with provider bloc

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-          emailField(),
-          passwordField(),
+          emailField(bloc),
+          passwordField(bloc),
           Container(margin: EdgeInsets.only(top: 25.0)),
           submitButton(),
         ],
@@ -16,7 +19,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget emailField() {
+  Widget emailField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.email,
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
@@ -32,7 +35,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget passwordField() {
+  Widget passwordField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.password,
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
