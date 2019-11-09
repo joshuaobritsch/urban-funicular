bf858cdca85557549c7afdd099be41d9a20c9db1
Joshua Obritsch <j.obritsch@gmail.com>
2019-11-09 12:11:36 +0100

    Implement submitValid method

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc with Validators {
   final _email = StreamController<String>();
@@ -7,6 +8,7 @@ class Bloc with Validators {
 
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid => Observable.combineLatest2(email, password, (e, p) => true);
 
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
